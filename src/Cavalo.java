619da86e1597109c487706bfdf6b6f30c9fcfb7b
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-24 20:54:38 -0300

    Feat: Adicionar método para o Cavalo

--- src/Cavalo.java
+++ src/Cavalo.java
@@ -3,5 +3,19 @@ public class Cavalo extends Peca {
         super(cor);
     }
 
+    @Override
+    public boolean movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino, Peca[][] tabuleiro) {
+        int deltaLinha = Math.abs(linhaDestino - linhaOrigem);
+        int deltaColuna = Math.abs(colunaDestino - colunaOrigem);
+        if (deltaLinha * deltaColuna == 2) {
+            return tabuleiro[linhaDestino][colunaDestino] == null || !tabuleiro[linhaDestino][colunaDestino].getCor().equals(cor);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return cor.equals("branco") ? "C" : "c";
+    }
 
 }
