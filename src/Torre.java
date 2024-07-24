e22e94e4408cf186df39729c6c0c46dffaebf793
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-24 20:43:20 -0300

    Feat:Definit método para movimento da torre

--- src/Torre.java
+++ src/Torre.java
@@ -4,5 +4,26 @@ public class Torre extends Peca {
     }
 
     @Override
-    public boolean movimentoValido(int linha)
+    public boolean movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino, Peca[][] tabuleiro){
+        if(linhaOrigem == linhaDestino || colunaOrigem == colunaDestino) {
+          int deltaLinha = Integer.signum(linhaDestino - linhaOrigem);
+          int deltaColuna = Integer.signum(colunaDestino - colunaOrigem);
+          int linha = linhaOrigem + deltaLinha;
+          int coluna = colunaOrigem + deltaColuna;
+          while(linha != linhaDestino || coluna != colunaDestino) {
+              if(tabuleiro[linha][coluna] != null) {
+                  return false;
+              }
+              linha += deltaLinha;
+              coluna += deltaColuna;
+          }
+          return tabuleiro[linha][coluna] == null || !tabuleiro[linhaDestino][colunaDestino].getCor().equals(cor);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return cor.equals("branco") ? "T" : "t";
+    }
 }
