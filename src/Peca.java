c9922b246e3973d876ac8fe30f7b032121121c26
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-23 22:03:23 -0300

    Feat: Criar classe para definir a cor das peças e os movimentos válidos

--- src/Peca.java
+++ src/Peca.java
@@ -1,3 +1,14 @@
 public abstract class Peca {
+    protected String cor;
+
+    public Peca(String cor) {
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public abstract boolean movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino, Peca[][] tabuleiro);
 
 }
