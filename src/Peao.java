a9a0f79f2d15baa1d27557eb552956773906de10
Lucas Eliasafe Mohr Vargas <139092655+LucasEliasafe@users.noreply.github.com>
2024-07-24 00:34:20 -0300

    Feat: Adicionar método para avançar primeiro duas casas, tomar em diagonal

--- src/Peao.java
+++ src/Peao.java
@@ -1,12 +1,29 @@
 public class Peao extends Peca {
-    public Peao() {
+    public Peao(String cor) {
         super(cor);
     }
 
     @Override
     public boolean movimentoValido(int linhaOrigem, int colunaOrigem, int linhaDestino, int colunaDestino, Peca[][] tabuleiro) {
         int direcao = cor.equals("branco") ? -1 : 1;
-        if(linhaDestino == linhaOrigem + direcao && colunaDestino == colunaOrigem + direcao) {}
+        if (linhaDestino == linhaOrigem + direcao && colunaDestino == colunaOrigem && tabuleiro[linhaDestino][colunaDestino] == null) {
+            return true;
+        }
+        if ((cor.equals("branco") && linhaOrigem == 6 || cor.equals("preto") && linhaOrigem == 1) &&
+                linhaDestino == linhaOrigem + 2 * direcao && colunaDestino == colunaOrigem &&
+                tabuleiro[linhaDestino][colunaDestino] == null && tabuleiro[linhaOrigem + direcao][colunaOrigem] == null) {
+            return true;
+        }
+
+        if (linhaDestino == linhaOrigem + direcao && Math.abs(colunaDestino - colunaOrigem) == 1 && tabuleiro[linhaDestino][colunaDestino] != null &&
+                !tabuleiro[linhaDestino][colunaDestino].getCor().equals(cor)) {
+            return true;
+        }
+        return false;
     }
 
+    @Override
+    public String toString() {
+        return cor.equals("branco") ? "P" : "p";
+    }
 }
